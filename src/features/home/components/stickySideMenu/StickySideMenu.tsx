fa9eee2a74f6756fc54cd87a78497a89f49036e9
Esra Tansuk <esra.balkis@outlook.com>
2025-04-19 17:36:41 +0300

    StickySideMenu bileşenine AboutSideWidget ve SocialMediaSideWidget eklendi. Bu değişiklik, yan menünün içeriğini zenginleştirerek kullanıcı deneyimini artırmayı hedefliyor. Ayrıca, bileşenin yapısı güncellenerek daha düzenli bir görünüm sağlandı.

--- src/features/home/components/stickySideMenu/StickySideMenu.tsx
+++ src/features/home/components/stickySideMenu/StickySideMenu.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { AboutSideWidget, SocialMediaSideWidget } from '../stickMenuWidget'
 
 export const StickySideMenu = () => {
   return (
-    <div>StickySideMenu</div>
+    <div className="top-24 left-0 w-full space-y-8">
+      <AboutSideWidget />
+      <SocialMediaSideWidget />
+    </div>
   )
 }
